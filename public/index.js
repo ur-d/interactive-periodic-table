a960905c53e5ef9bd9689c4a2722c6161abe84ee
uri <drcreeper7mc@gmail.com>
2022-02-07 16:47:21 +0100

    mix old

---
+++ public/index.js
@@ -0,0 +1,1 @@
+// AtomicNumber;Element;Symbol;AtomicMass;Period;Group;Phase;Radioactive;Natural;Metal;Nonmetal;Metalloid;Type;MeltingPoint;BoilingPoint;Discoverer;Year;NumberofShells;NumberofValence
